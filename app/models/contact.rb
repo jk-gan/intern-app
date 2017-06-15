d4d30996eafa3858dd3eaef04587e4ae4474c48f
Gan Jun Kai <kuhn96@gmail.com>
2017-06-15 19:17:48 +0800

    Validates valid email for company

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -21,5 +21,6 @@ class Contact < ApplicationRecord
 
   validates :name, presence: true
   validates :role, presence: true
-	validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
