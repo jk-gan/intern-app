de246eb136260da43840f9da34bfaa3cb1f995c1
Gan Jun Kai <kuhn96@gmail.com>
2017-07-11 11:23:39 +0800

    Setup profile with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  accepts_nested_attributes_for :profile, reject_if: :all_blank, allow_destroy: true
 end
